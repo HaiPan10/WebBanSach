4ad0eda0c4ace364e4c40b7677871838a97c5453
PlaIsMe <phongvulai96@gmail.com>
2022-11-20 20:53:46 +0700

    added_template

--- app/index.py
+++ app/index.py
@@ -3,7 +3,9 @@ from app import app
 # Import render_template để dùng render_template
 from flask import render_template
 # Import modul utils để dùng hàm load_
-import  utils
+import utils
+# Dùng request để đổ product theo cate_id
+from flask import  request
 
 
 # Định nghĩa đường dẫn
@@ -11,15 +13,29 @@ import  utils
 def home():
     # Đổ dữ liệu category
     cates = utils.load_categories()
-    return render_template('index.html', categories = cates)
+    return render_template('index.html', categories=cates)
 
 
 # Chuyển trang product
 @app.route("/products")
 def product_list():
-    #Đổ dữ liệu product
-    products = utils.load_products()
-    return render_template('products.html', products = products)
+    #Đổ dữ liệu theo cate_id
+    cate_id = request.args.get("category_id")
+    kw = request.args.get("keyword")
+    from_price = request.args.get("from_price")
+    to_price = request.args.get("to_price")
+
+    products = utils.load_products(cate_id=cate_id, kw=kw, from_price=from_price, to_price=to_price)
+
+    return render_template('products.html', products=products)
+
+
+# Cấu hình trang chi tiết sản phẩm
+@app.route("/products/<int:product_id>")
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id)
+
+    return  render_template('product_detail.html', product=product)
 
 
 # Chạy trang web
--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@
 import json
 # Lấy đường dẫn tuyệt đối
 import os
-from app import  app
+from app import app
 
 
 # Hàm đọc json
@@ -17,5 +17,29 @@ def load_categories():
 
 
 # Đọc product
-def load_products():
-    return read_json(os.path.join(app.root_path, 'data/products.json'))
+def load_products(cate_id=None, kw=None, from_price=None, to_price=None):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+
+    # Đoạn này sẽ sửa đổi sau
+    if cate_id:
+        products = [p for p in products if p['category_id'] == int(cate_id)]
+
+    if kw:
+        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+
+    if from_price:
+        products = [p for p in products if p['price'] >= float(from_price)]
+
+    if to_price:
+        products = [p for p in products if p['price'] <= float(to_price)]
+
+    return products;
+
+
+# Lấy products từ mã
+def get_product_by_id(product_id):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+
+    for p in products:
+        if p['id'] == product_id:
+            return p
