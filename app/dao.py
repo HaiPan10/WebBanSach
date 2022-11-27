fa249abca1c6d8d9492de7dd40ac5218ecfe0832
Hai Phan <phanthanhhai3317@gmail.com>
2022-11-27 11:36:10 +0700

    Create dao.py

---
+++ app/dao.py
@@ -0,0 +1,35 @@
+from app.models import UserAccount, Books, Categories, Orders, OrderDetails, UserRole
+from app import db
+import hashlib
+
+
+def load_books(cate_id=None, keyword=None):
+    query = Books.query
+    if cate_id:
+        query = query.filter(Books.category_id.__eq__(cate_id))
+
+    if keyword:
+        query = query.filter(Books.book_name.contains(keyword))
+
+    return query.all()
+
+
+def load_categories():
+    return Categories.query.all()
+
+
+def load_order_by_id(order_id):
+    return Orders.query.get(order_id)
+
+
+def load_orders():
+    return Orders.query.all()
+
+
+def create_account(name, username, password, avatar):
+    pw = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    user = UserAccount(name=name, username=username, password=pw, avatar=avatar, user_role=UserRole.USER)
+
+
+def get_user_by_id(user_id):
+    return UserAccount.query.get(user_id)
